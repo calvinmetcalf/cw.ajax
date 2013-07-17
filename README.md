55c4fdb6f53986297d7abe1a798dbdf6ec3638be
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-07-17 09:48:10 -0400

    all set

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ cw.ajax
 =======
 
 ajax plugin for [communist](https://github.com/calvinmetcalf/communist)
+
+usage 
+
+```javascript
+var worker = cw.ajax(after,notjson);
+worker('url').then(successFunction,errorFunction);
+worker.close();
+```
+
+After is a function to be done before sending it back takes the response and the callback as arguments, defaults to
+
+```javascript
+function(a,cb){
+    cb(a);
+}
+```
+
+notjson is a boolian, if you set it to true then the response won't be parsed as json.
--- cw.ajax.js
+++ cw.ajax.js
@@ -1,12 +1,12 @@
 cw.ajax = function(after,notjson){
     var obj={ajax:function (url, _cb) {
-		var that = this;
+    	var that = this;
 		var request = new XMLHttpRequest();
 		request.open("GET", url);
 			request.onreadystatechange = function() {
 				var _resp;
 				if (request.readyState === 4 && request.status === 200) {
-					_resp = that.after(!that.notjson?JSON.parse(request.responseText):request.responseText);
+					_resp = that.after(!that.notjson?JSON.parse(request.responseText):request.responseText,_cb);
 					if(typeof _resp!=="undefined"){
 						_cb(_resp);
 						}
@@ -17,13 +17,12 @@ cw.ajax = function(after,notjson){
 			};
 		request.send();
 	}};
-	obj.after = after||function(a){return a;};
+	obj.after = after||function(a, cb){cb(a);};
 	obj.notjson = notjson||false;
     var worker = cw(obj);
-    return {
-        ajax:function(url){
-            return worker.ajax(cw.makeUrl(url));
-        },
-        close:worker.close
+    var ajax = function(url){
+        return worker.ajax(cw.makeUrl(url));
     };
+    ajax.close = worker.close;
+    return ajax;
 };
